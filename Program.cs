027e3384ecc2f7bd60392dc19081a8f57b40e387
botea <botea@IULIA>
2022-11-30 13:08:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problema19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
